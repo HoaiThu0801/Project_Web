54b8d77d7b9a40b9a5d869030fcd0b242552e173
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-11-13 23:08:14 +0700

    Thêm vào file javascript để tắt mở popup form

---
+++ Scripts/CreateStore.js
@@ -0,0 +1,22 @@
+﻿var flaq = true;
+function openPopupForm() {
+
+    if (flaq == false) {
+        document.getElementById("signup-content").style.display = "none";
+        flaq = true;
+    }
+    else {
+        document.getElementById("signup-content").style.display = "block";
+        flaq = false;
+    }
+}
+function closePopupForm() {
+    if (flaq == false) {
+        document.getElementById("signup-content").style.display = "none";
+        flaq = true;
+    }
+    else {
+        document.getElementById("signup-content").style.display = "none";
+        flaq = false;
+    }
+}
