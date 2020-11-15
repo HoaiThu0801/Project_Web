f73abc1cf0ff1879279b41405a4cb38c16bb531e
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-11-16 01:07:24 +0700

    delete validate at username

--- Models/FogotPassWord.cs
+++ Models/FogotPassWord.cs
@@ -8,12 +8,11 @@ namespace Project_Web.Models
 {
     public class FogotPassWord
     {
-        [MinLength(8, ErrorMessage = "Tên ít nhất 8 kí tự")]
-        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Tên của bạn phải chứa chữ cái viết thường hoặc hoa, VD: Huutuong")]
         public string Username { get; set; }
         [MinLength(8, ErrorMessage = "Mật khẩu phải nhập ít nhất 8 kí tự")]
         [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", ErrorMessage = "Mật khẩu phải chứa chữ in hoa, số hoặc ký tự đặc biệt")]
         [Required(ErrorMessage = "Mật khẩu không được trống!")]
         public string Password { get; set; }
+
     }
 }
