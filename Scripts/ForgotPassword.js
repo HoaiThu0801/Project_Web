2874c367a048755ae1b625910c3db849b85fc2d3
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-11-16 01:09:12 +0700

    Add file javascript ForgotPassword Add event key up for id Username, Password

---
+++ Scripts/ForgotPassword.js
@@ -0,0 +1,38 @@
+﻿$('#re_password').keyup(function () {
+    var password = $('#Password').val();
+    var re_password = $('#re_password').val();
+    if (re_password != password) {
+        $('#showError').html('Mật khẩu không trùng khớp');
+        $('#showError').css('color', 'black');
+        return false;
+    } else {
+        $('#showError').html('');
+        return true
+    }
+});
+$('#Username').keyup(function () {
+    $('#IsNotUsername').html('');
+    var username = $('#Username').val();
+    if (username == '') {
+        $('#IsNotUsername').html('Username không được trống');
+        $('#IsNotUserName').css('color', 'black');
+        return false;
+    }
+    else {
+        $('#IsNotUsername').html('');
+        return true;
+    }
+});
+$('#Password').keyup(function () {
+    $('#IsNotPassword').html('');
+    var username = $('#Password').val();
+    if (username == '') {
+        $('#IsNotPassword').html('Mật khẩu không được trống');
+        $('#IsNotPassword').css('color', 'black');
+        return false;
+    }
+    else {
+        $('#IsNotPassword').html('');
+        return true;
+    }
+});
