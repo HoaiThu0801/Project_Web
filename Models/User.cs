d12a1ffcf35d52ca827c4e155cb6f9b135f3b6bf
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2020-11-23 07:52:55 +0700

    Thu: modify

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Web;
-using System.Linq;
+
 namespace Project_Web.Models
 {
     public partial class User
