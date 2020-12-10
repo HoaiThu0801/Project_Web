adf1485241c80fd8ff2b71e7a34989133144580f
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-12-10 13:33:42 +0700

    Tuong: Add OrderManagement

---
+++ Controllers/OrderManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Web.Controllers
+{
+    public class OrderManagementController : Controller
+    {
+        // GET: OrderManagement
+        public ActionResult OrderManagement()
+        {
+            return View();
+        }
+    }
+}
