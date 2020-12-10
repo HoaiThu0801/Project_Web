ac583007a570c9297ab554f702b0260683acff27
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-12-10 16:40:41 +0700

    Tuong: Update Module OrderManagement

--- Scripts/OrderManagement.js
+++ Scripts/OrderManagement.js
@@ -2,16 +2,12 @@
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.opacity = 0;
-        tabcontent[i].style.visibility = "hidden";
-        tabcontent[i].style.height = 0;
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(orderName).style.opacity = 1;
-    document.getElementById(orderName).style.visibility = "visible";
-    document.getElementById(orderName).style.height = "unset";
+    document.getElementById(orderName).style.display = "block"; 
     evt.currentTarget.className += " active";
 }
