02df84a6a6837fed854144904e8ce42ec91f8206
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-12-10 11:09:53 +0700

    Tuong: Update View OrderProducts

--- Scripts/OrderProducts.js
+++ Scripts/OrderProducts.js
@@ -1,3 +1,22 @@
-﻿$('#update-button').click(function () {
+﻿$('#update-button-information').click(function () {
     $(window).attr('location', '../OrderProducts/Shipping');
 });
+$('#update-button-products').click(function () {
+    $(window).attr('location', '../Home/ShoppingCart');
+});
+
+$(document).ready(function () {
+    var flaqCheckBox = true;
+    $('#CheckBox').click(function () {
+        if (flaqCheckBox == true) {
+            $(this).addClass('fa-check-square');
+            $('.fa-check-square').css('color', 'darkblue');
+            flaqCheckBox = false;
+        }
+        else {
+            $(this).removeClass('fa-check-square');
+            $('.fa-square-o').css('color', 'black');
+            flaqCheckBox = true
+        }
+    });
+})
