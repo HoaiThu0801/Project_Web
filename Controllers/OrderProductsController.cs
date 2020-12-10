57584e5c304369648bc0880a3115237f1f1de424
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2020-12-10 15:47:57 +0700

    Thu: Display order item, add add_user(conn)

--- Controllers/OrderProductsController.cs
+++ Controllers/OrderProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,12 +12,23 @@ namespace Project_Web.Controllers
     {
         Database_PorridgeSellingManagementStoreEntities _db = new Database_PorridgeSellingManagementStoreEntities();
         // GET: OrderProducts
+        [Authorize_userController]
         public ActionResult OrderProducts()
         {
             return View();
         }
+        [HttpGet]
+        [Authorize_userController]
         public ActionResult Shipping()
         {
+            return View();
+        }
+        [HttpPost()]
+        [Authorize_userController]
+        public ActionResult Shipping(FormCollection form)
+        {
+            User user = new User();
+            user = Session["User"] as User;
             return View();
         }
         #region LoadData
@@ -26,10 +38,36 @@ namespace Project_Web.Controllers
             Province pro = _db.Provinces.SingleOrDefault(n => n.Name == ProvinceName);
             if (pro != null)
             {
+                List<string> namedistricts = new List<string>();
+                string name;
                 var districts = (from d in _db.Districts
                                  where d.ProvinceId == pro.Id
-                                 select d.Name);
-                return Json(districts.ToList(), JsonRequestBehavior.AllowGet);
+                                 select new { d.Name, d.Type}).ToList();
+                foreach (var d in districts)
+                {
+                    name = d.Type + " " + d.Name;
+                    namedistricts.Add(name);
+                }
+                return Json(namedistricts, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult LoadWard(string DistrictName)
+        {
+            District dis = _db.Districts.SingleOrDefault(n => n.Name == DistrictName);
+            if (dis != null)
+            {
+                List<string> nameward = new List<string>();
+                string name;
+                var wards = (from w in _db.Wards
+                                 where w.DistrictID == dis.Id
+                                 select new { w.Name, w.Type }).ToList();
+                foreach (var w in wards)
+                {
+                    name = w.Type + " " + w.Name;
+                    nameward.Add(name);
+                }
+                return Json(nameward, JsonRequestBehavior.AllowGet);
             }
             return Json(false, JsonRequestBehavior.AllowGet);
         }
--- Scripts/Shipping.js
+++ Scripts/Shipping.js
@@ -10,10 +10,46 @@
             success: function (res) {
                 $('#DistrictSelect').html("");
                 $("#DistrictSelect").append(("<option disabled selected>" + "Chọn Quận/Huyện" + "</option>"));
-                $.each(response, function (key, item) {
+                $.each(res, function (key, item) {
                     $("#DistrictSelect").append("<option>" + item + "</option>");
                 });
             }
         })
     })
+})
+$(document).ready(function () {
+    $("#DistrictSelect").change(function () {
+        var temp = $("#DistrictSelect").val();
+        var spaceIndex = temp.search(" ");
+        var districtname = temp.slice(spaceIndex + 1, temp.length);
+        $.ajax({
+            type: "get",
+            url: "/OrderProducts/LoadWard",
+            data: {
+                DistrictName: districtname
+            },
+            success: function (res) {
+                $('#WardSelect').html("");
+                $("#WardSelect").append(("<option disabled selected>" + "Chọn Phường/Xã" + "</option>"));
+                $.each(res, function (key, item) {
+                    $("#WardSelect").append("<option>" + item + "</option>");
+                });
+            }
+        })
+    })
+})
+$(document).ready(function () {
+    $("#address-form").submit(function (event) {
+        event.preventDefault();
+        var url = $(this).attr("action");
+        var form = $(this).serialize();
+        $.ajax({
+            type: 'post',
+            url: url,
+            data: form,
+            success: function (res) {
+            },
+
+        })
+    });
 })
