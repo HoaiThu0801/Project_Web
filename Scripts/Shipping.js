ba821ddf63983144928f9610fb2499254676abd5
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-12-11 10:32:50 +0700

    Tuong: Update module Shipping, when click button Sửa load data user, when click Thêm địa chỉ mới reset form, when click button Hủy reset form

--- Scripts/Shipping.js
+++ Scripts/Shipping.js
@@ -53,21 +53,39 @@ $(document).ready(function () {
                     $(window).attr('location', '../OrderProducts/Shipping');
                 }
             },
-
         })
     });
 })
 
-$('#openForm').click(function () {
-    $('#form-address').css('opacity', '1');
-    $('#form-address').css('visibility', 'visible');
-    $('#form-address').css('height', 'unset');
-    $('#form-address').css('width', '100%');
-});
-$('#Cancel').click(function () {
-    $('#form-address').css('opacity', '0');
-    $('#form-address').css('visibility', 'hidden');
-    $('#form-address').css('height', '0');
-    $('#form-address').css('width', '0');
-    $('#address-form').trigger("reset");
+$(document).ready(function () {
+    var fullname = $('#Fullname').val();
+    var phone = $('#PhoneNumber').val();
+    var street = $('#Street').val();
+    $('#update-button').click(function () {
+        $('#form-address').css('opacity', '1');
+        $('#form-address').css('visibility', 'visible');
+        $('#form-address').css('height', 'unset');
+        $('#form-address').css('width', '100%');
+        $('html, body').animate({ scrollTop: $('#form-address').offset().top }, 'slow');
+        $('#Fullname').val(fullname);
+        $('#PhoneNumber').val(phone);
+        $('#Street').val(street);
+    });
+    $('#openForm').click(function () {
+        $('#form-address').css('opacity', '1');
+        $('#form-address').css('visibility', 'visible');
+        $('#form-address').css('height', 'unset');
+        $('#form-address').css('width', '100%');
+        $('html, body').animate({ scrollTop: $('#form-address').offset().top }, 'slow');
+        $('#Fullname').val('');
+        $('#PhoneNumber').val('');
+        $('#Street').val('');
+    });
+    $('#Cancel').click(function () {
+        $('#form-address').css('opacity', '0');
+        $('#form-address').css('visibility', 'hidden');
+        $('#form-address').css('height', '0');
+        $('#form-address').css('width', '0');
+        $('#address-form').trigger("reset");
+    });
 });
