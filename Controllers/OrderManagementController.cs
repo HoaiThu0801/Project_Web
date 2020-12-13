30e2a586556acec561d077fcdd650ff8e286c902
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2020-12-14 00:02:16 +0700

    Thu: Load Order in All (Admin)

--- Controllers/OrderManagementController.cs
+++ Controllers/OrderManagementController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project_Web.Models;
+using PagedList;
 
 namespace Project_Web.Controllers
 {
+    
     public class OrderManagementController : Controller
     {
+        Database_PorridgeSellingManagementStoreEntities _db = new Database_PorridgeSellingManagementStoreEntities();
         // GET: OrderManagement
-        public ActionResult OrderManagement()
+        public ActionResult OrderManagement(int? page)
         {
-            return View();
+            if (page == null) page = 1;
+            var billDetails = (from bd in _db.Bills
+                        select bd).OrderBy(x => x.IDBill);
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            return View(billDetails.ToPagedList(pageNumber, pageSize));
         }
     }
 }
