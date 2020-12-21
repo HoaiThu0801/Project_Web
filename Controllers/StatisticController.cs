118b9636fb419fb1967d54e6d6f739904933fda3
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2020-12-21 22:51:12 +0700

    Tuong: Update add tab statistic in HomePage for Admin, change format double in statistic

--- Controllers/StatisticController.cs
+++ Controllers/StatisticController.cs
@@ -9,6 +9,7 @@ using System.Dynamic;
 
 namespace Project_Web.Controllers
 {
+    [Authorize_seller_adminController]
     public class StatisticController : Controller
     {
         Database_PorridgeSellingManagementStoreEntities _db = new Database_PorridgeSellingManagementStoreEntities();
@@ -16,64 +17,6 @@ namespace Project_Web.Controllers
         // GET: Statistoc
         public ActionResult Statistic()
         {
-            return View();
-        }
-        public ActionResult Statistic_QuantityImportProduct()
-        {
-            //List<DataPoint_Label_y> dataPoints = new List<DataPoint_Label_y>();
-
-            //var warehouses = (from s in _db.Stores
-            //                  join wh in _db.Warehouses on s.IDStore equals wh.IDStore
-            //                  select wh).ToList();
-            //foreach (var wh in warehouses)
-            //{
-            //    var warehouseDetails = (from whd in _db.WarehouseDetails
-            //                            where whd.IDWarehouse == wh.IDWarehouse
-            //                            select whd);
-            //    if (warehouseDetails != null)
-            //    {
-            //        double sum = 0;
-            //        foreach (var temp in warehouseDetails)
-            //        {
-            //            sum = sum + Double.Parse(temp.Quantity.ToString());
-            //        }
-            //        dataPoints.Add(new DataPoint_Label_y(wh.WarehouseName, sum));
-            //    }
-            //    else
-            //    {
-            //        dataPoints.Add(new DataPoint_Label_y(wh.WarehouseName, 0));
-            //    }
-
-            //}
-            //ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
-            return View();
-        }
-        public ActionResult Statistic_ReveneuProduct()
-        {
-            //List<DataPoint_Label_y> dataPoint_Label_Y = new List<DataPoint_Label_y>();
-            //var store = (from s in _db.Stores
-            //             select s).ToList();
-            //foreach (var s in store)
-            //{
-            //    var bill = (from b in _db.Bills
-            //                join ot in _db.OrderTracks on b.IDBill equals ot.IDBill
-            //                where b.IDStore == s.IDStore && ot.IDOrderStatse == "OS-05"
-            //                select b).ToList();
-            //    if (bill.Count == 0)
-            //    {
-            //        dataPoint_Label_Y.Add(new DataPoint_Label_y(s.StoreName, 0));
-            //    }
-            //    else
-            //    {
-            //        float sum = 0;
-            //        foreach (var temp in bill)
-            //        {
-            //            sum = sum + float.Parse(temp.Total.ToString());
-            //        }
-            //        dataPoint_Label_Y.Add(new DataPoint_Label_y(s.StoreName, sum));
-            //    }
-            //}
-            //ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoint_Label_Y);
             return View();
         }
     }
