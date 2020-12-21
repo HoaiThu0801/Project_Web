bb9cc71b7a6402842920e8481c1aabccf1d81e11
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2020-12-21 08:02:41 +0700

    Thu: Code Chart TotalNumberofProducts

---
+++ Models/DataPoint_Name_y.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Project_Web.Models
+{
+    [DataContract]
+    public class DataPoint_Name_y
+    {
+
+		public DataPoint_Name_y(string name, double y)
+		{
+			this.Name = name;
+			this.Y = y;
+		}
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "name")]
+		public string Name = "";
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "y")]
+		public Nullable<double> Y = null;
+	}
+}
