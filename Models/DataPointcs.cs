a2869a405012aee615f9bd2dcb9660849aae715a
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2020-12-31 22:35:43 +0700

    Thu: StatisticReveneuover30days, Fix bug dish null in OrderManagement

---
+++ Models/DataPointcs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Project_Web.Models
+{
+	//DataContract for Serializing Data - required to serve in JSON format
+	[DataContract]
+	public class DataPoint
+	{
+		public DataPoint(double x, double y)
+		{
+			this.x = x;
+			this.Y = y;
+		}
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "x")]
+		public Nullable<double> x = null;
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "y")]
+		public Nullable<double> Y = null;
+	}
+}
